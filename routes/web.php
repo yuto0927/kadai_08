9b9795661f4131503cc831e8b89166935a04664a
yuto <yuto.ogami5@gmail.com>
2020-08-07 07:13:43 +0000

    kadai

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,8 @@ Route::get('/', function () {
 Route::group(['prefix' => 'admin'], function() {
     Route::get('news/create', 'Admin\NewsController@add');
 });
+Route::get('XXX', 'AAAController@bbb');
+Route::group(['prefix' => 'admin'], function() {
+     Route::get('profile/create', 'Admin\ProfileController@add');
+     Route::get('profile/edit', 'Admin\ProfileController@delete');
+});
