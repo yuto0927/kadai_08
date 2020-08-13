c704067251c215536199b522063f1bd0ed3e2cf1
yuto <yuto.ogami5@gmail.com>
2020-08-13 03:20:31 +0000

    kadai11

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,13 +1,16 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+{{-- layouts/admin.blade.phpを読み込む --}}
+@extends('layouts.admin')
 
-        <title>kadai10</title>
-    </head>
-    <body>
-        <h1>練習create</h1>
-    </body>
-</html>
+{{-- admin.blade.phpの@yield('title')に'ニュースの新規作成'を埋め込む --}}
+@section('title', 'プロフィールの作成')
+
+{{-- admin.blade.phpの@yield('content')に以下のタグを埋め込む --}}
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>My プロフィール</h2>
+            </div>
+        </div>
+    </div>
+@endsection
