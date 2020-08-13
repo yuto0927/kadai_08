2640b9aa317fb086c42c4f4425dea711f0e79f37
yuto <yuto.ogami5@gmail.com>
2020-08-13 06:47:20 +0000

    kadai12

--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::group(['prefix' => 'admin'], function() {
-    Route::get('news/create', 'Admin\NewsController@add');
+    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
 });
 Route::get('XXX', 'AAAController@bbb');
 Route::group(['prefix' => 'admin'], function() {
-     Route::get('profile/create', 'Admin\ProfileController@add');
-     Route::get('profile/edit', 'Admin\ProfileController@edit');
+     Route::get('profile/create', 'Admin\ProfileController@add')->middleware('auth');
+     Route::get('profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
 });
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
